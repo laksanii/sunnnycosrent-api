d620597b77f1e0fe57ecb633ce2b4f6232ccb395
Prasetyo Adi Pratama Nugroho <prazetyo111@gmail.com>
2023-02-15 21:34:56 +0700

    Create CostumePict seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
+use App\Models\Costume;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -35,6 +37,19 @@ class DatabaseSeeder extends Seeder
             DB::table('categories')->insert([
                 'name' => $category
             ]);
+        }
+
+        $faker = Faker::create();
+
+        $costumes = Costume::all();
+
+        foreach ($costumes as $costume) {
+            for ($i = 0; $i < 3; $i++) {
+                DB::table('costume_picts')->insert([
+                    'path' => $faker->imageUrl(640, 480, 'animals', true),
+                    'costume_id' => $costume->id,
+                ]);
+            }
         }
     }
 }
